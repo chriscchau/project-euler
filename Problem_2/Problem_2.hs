3f85d8ec087f3774ee4fcefe713fd951b59eafff
Chris Chau <chrisc.chau@gmail.com>
2013-03-19 10:00:57 +0800

    update Problem 2

--- Problem_2/Problem_2.hs
+++ Problem_2/Problem_2.hs
@@ -1,6 +1,5 @@
 main = print (sum [z | z<-(takeWhile (<4000000) [fibonacci x | x<-[1..]]),even z])
 
-fibonacci x = case x of
-    1 -> 1
-    2 -> 2
-    _ -> (fibonacci (x-2)) + (fibonacci (x-1))
+fibonacci x | x == 1 = 1
+            | x == 2 = 2
+            | otherwise = (fibonacci (x-2)) + (fibonacci (x-1))
